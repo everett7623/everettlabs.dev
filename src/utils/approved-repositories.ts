38e6e7db983100775e87246977d4c30ff248212b
everettlabs <everett7623@gmail.com>
2026-08-03 14:57:49 +0800

    feat: 新增 PicSift 与 LogoLens 项目

--- src/utils/approved-repositories.ts
+++ src/utils/approved-repositories.ts
@@ -4,6 +4,8 @@ export const approvedRepositories = [
   'everett7623/FavGrove',
   'everett7623/Citeoryx',
   'everett7623/LinkVitals',
+  'everett7623/PicSift',
+  'everett7623/LogoLens',
   'everett7623/Nezha-cleaner',
   'everett7623/debian-auto-upgrade',
   'everett7623/vps_scripts',
